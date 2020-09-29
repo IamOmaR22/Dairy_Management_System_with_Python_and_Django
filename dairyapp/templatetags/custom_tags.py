b95b9873e9243445c212398c511da9a984fb78ff
Md. Omar Faruk <iamomar022@gmail.com>
2020-09-29 07:07:34 +0600

    Templatags Removed

--- dairyapp/templatetags/custom_tags.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library()
-
-
-@register.simple_tag(name="multiply")
-def multiply(qty, unit_price, *args, **kwargs):
-    # you would need to do any localization of the result here
-    return qty * unit_price

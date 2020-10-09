3d8902e34f12403339f5263ad2a53b69dc800a58
Md. Omar Faruk <iamomar022@gmail.com>
2020-10-09 20:32:01 +0600

    Email Done For Contact Us

--- MyDairy/settings.py
+++ MyDairy/settings.py
@@ -146,7 +146,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL=False
